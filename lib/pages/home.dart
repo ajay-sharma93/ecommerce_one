b7c8abcf820fb00cb313108a07558e13fe490c06
ajaysharma <sharma.ajaya2019@gmail.com>
2025-06-05 13:06:40 +0545

    when the svg image is clicked,the image shows black color in the screen where i used GestureDetector and onTap funtion

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,7 +14,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
 
-  bool icecream=false, pizza=false,salad=false,burger=false;
+  bool ice=false, pizza=false,salad=false,burger1=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,36 +49,89 @@ class _HomeState extends State<Home> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: SvgPicture.asset('images/svg/ice.svg',height: 50,width: 50,),
+              GestureDetector(
+                onTap: (){
+                  ice=true;
+                  burger1=false;
+                  salad=false;
+                  pizza=false;
+                  setState(() {
+                    
+                  });
+
+                },
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    decoration: BoxDecoration(color: ice?Colors.black:Colors.white,borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.all(8),
+                    // ignore: deprecated_member_use
+                    child: SvgPicture.asset('images/svg/ice.svg',height: 40,width: 40,fit: BoxFit.cover,color: ice?Colors.white:Colors.black,),
+                  ),
                 ),
               ),
-              Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: SvgPicture.asset('images/svg/burger1.svg',height: 50,width: 50,),
+              GestureDetector(
+                onTap:(){
+                  ice=false;
+                  burger1=true;
+                  salad=false;
+                  pizza=false;
+                  setState(() {
+                    
+                  });
+                },
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    decoration: BoxDecoration(color: burger1?Colors.black:Colors.white,borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.all(8),
+                    // ignore: deprecated_member_use
+                    child: SvgPicture.asset('images/svg/burger1.svg',height: 40,width: 40,fit: BoxFit.cover,color: burger1?Colors.white:Colors.black,),
+                  ),
                 ),
               ),
-              Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: SvgPicture.asset('images/svg/salad.svg',height: 50,width: 50,),
+              GestureDetector(
+                onTap: (){
+                  ice=false;
+                  burger1=false;
+                  salad=true;
+                  pizza=false;
+                  setState(() {
+                    
+                  });
+                },
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    decoration: BoxDecoration(color: salad?Colors.black:Colors.white,borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.all(8),
+                    // ignore: deprecated_member_use
+                    child: SvgPicture.asset('images/svg/salad.svg',height: 40,width: 40,fit: BoxFit.cover,color: salad?Colors.white:Colors.black,),
+                  ),
                 ),
               ),
-              Material(
-                elevation: 5.0,
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: SvgPicture.asset('images/svg/pizza.svg',height: 50,width: 50,),
+              GestureDetector(
+                onTap: (){
+                   ice=false;
+                  burger1=false;
+                  salad=false;
+                  pizza=true;
+                  setState(() {
+                    
+                  });
+                },
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    decoration: BoxDecoration(color: pizza?Colors.black:Colors.white,borderRadius: BorderRadius.circular(8)),
+                    padding: EdgeInsets.all(8),
+                    // ignore: deprecated_member_use
+                    child: SvgPicture.asset('images/svg/pizza.svg',height: 40,width: 40,fit: BoxFit.cover,color: pizza?Colors.white:Colors.black,),
+                  ),
                 ),
               ),
             ],

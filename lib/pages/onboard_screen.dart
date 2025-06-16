674fd6375998a299493837d376ce4ef411bdf27a
ajaysharma <sharma.ajaya2019@gmail.com>
2025-06-16 22:46:48 +0545

    onbording screen is added

--- lib/pages/onboard_screen.dart
+++ lib/pages/onboard_screen.dart
@@ -8,8 +8,38 @@ class OnboardScreen extends StatefulWidget {
 }
 
 class _OnboardScreenState extends State<OnboardScreen> {
+
+
+  int currentIndex=0;
+  late PageController _controller;
+
+
+  @override
+
+  void initState(){
+    _controller=PageController(initialPage: 0);
+    super.initState();  
+  }
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(children: [
+        PageView.builder(
+          controller: _controller,  
+          i      
+          itemBuilder: (_,i){
+          return Padding(padding: EdgeInsets.all(20),
+          child: Column(children: [],),
+          );
+        })
+      ],),
+    );
   }
 }

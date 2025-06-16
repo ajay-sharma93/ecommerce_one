9c0194d2f4c1a4eac6ca5e1f07ab7f31b610b55c
ajaysharma <sharma.ajaya2019@gmail.com>
2025-06-16 22:48:36 +0545

    content model is addded to place the required image and description

---
+++ lib/widgets/content_model.dart
@@ -0,0 +1,11 @@
+class UnbordingContent{
+  String image;
+  String title;
+  String description;
+
+  UnbordingContent({required this.image,required this.title, required this.description});
+}
+
+List<UnbordingContent> contents=[
+  UnbordingContent(image: "images/screen1.png", title: 'Select from our\n Best men', description: 'Pick your food from our menu\n  more than 35 times')
+];

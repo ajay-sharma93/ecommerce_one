668d771bf07bf2c2774a9c072598c289a593e96d
ajaysharma <sharma.ajaya2019@gmail.com>
2025-06-26 23:00:30 +0545

    wallet screen modified!!

--- lib/pages/wallet.dart
+++ lib/pages/wallet.dart
@@ -14,7 +14,10 @@ class _WalletState extends State<Wallet> {
     return Scaffold(
     body: Container(
      margin: EdgeInsets.only(top: 60.0),
-     child: Column(children: [
+     child: Column(
+     
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
       Material(
         elevation: 2.0,
         child: Container(
@@ -24,10 +27,91 @@ class _WalletState extends State<Wallet> {
           Container(
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(color: Color(0xFFF2F2F2)),
-            child: Row(            
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row( 
+                           
+                children: [
+              Image.asset("assets/images/wallet.png",width: 60,height:60,fit:BoxFit.cover,),
+              SizedBox(width: 40.0,),
+              Column(
+                crossAxisAlignment:CrossAxisAlignment.start,
+                children: [
+                Text("Your Wallet",style: AppWidget.boldTextFieldStyle(),),
+                SizedBox(height: 5.0,),
+                Text("\$"+ "1000",style: AppWidget.semiBoldTextFieldStyle(),),
+              ],)
+                        ],
+                        ),
+            ),),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text("Add Money",style: AppWidget.semiBoldTextFieldStyle(),),
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-            Image.asset("assets/images/wallet.png",width: 100,height:60,fit:BoxFit.cover,),
-                      ],),)
+
+              GestureDetector(
+                onTap: (){},
+                child: Material(
+                  
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFE9E2E2)),borderRadius: BorderRadius.circular(10)
+                    ),
+                  child: Text("\$"+"100",style: AppWidget.boldTextFieldStyle(),),),
+                ),
+              ),
+              Material(
+                elevation: 5,
+                 borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFE9E2E2)),borderRadius: BorderRadius.circular(10)
+                  ),
+                child: Text("\$"+"500",style: AppWidget.boldTextFieldStyle(),),),
+              ),
+              Material(
+                elevation: 5,
+                 borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFE9E2E2)),borderRadius: BorderRadius.circular(10)
+                  ),
+                child: Text("\$"+"1000",style: AppWidget.boldTextFieldStyle(),),),
+              ),
+              Material(
+                elevation: 5,
+                 borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFE9E2E2)),borderRadius: BorderRadius.circular(10)
+                  ),
+                child: Text("\$"+"1500",style: AppWidget.boldTextFieldStyle(),),),
+              ),
+            
+            ],),
+          SizedBox(height: 20,),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 3),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 0, 78, 157)
+            ),
+          )
     ],),), 
     );
   }
